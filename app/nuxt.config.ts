a2de13daf14ed3acb5cdec188c1454dd7ca9a98d
Leonel Dias <leonel.j.dias@gmail.com>
2024-05-21 15:20:54 +0100

    Update Dockerfile, package.json, and API routes to handle missing items in ship data processing

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,76 +2,149 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  build: {
-    transpile: ["vuetify"],
-  },
-  experimental: {
-    watcher: "chokidar",
-  },
-  css: ["maplibre-gl/dist/maplibre-gl.css", "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css", "maplibre-gl-basemaps/lib/basemaps.css"],
-  runtimeConfig: {
-    public: {
-      REALTIME_URL: process.env.REALTIME_URL || "http://localhost:8080",
-      EXP_API_URL: process.env.EXP_API_URL || "http://localhost:8080",
-      API_URL: process.env.API_URL || "http://localhost:8082/api",
-      MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
-      OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
+  $production: {
+    build: {
+      transpile: ["vuetify"],
     },
-  },
-  googleSignIn: {
-    clientId: "CLIENT ID OBTAINED FROM GOOGLE API CONSOLE",
-  },
-  modules: [
-    "@sidebase/nuxt-auth",
-    "nuxt-vue3-google-signin",
-    "nuxt3-vuex-module",
-    "nuxt-gtag",
-    "nuxt-svgo",
-    "@nuxtjs/google-fonts",
-    (_options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }));
-      });
+    experimental: {
+      watcher: "chokidar",
     },
-  ],
-  auth: {
-    baseURL: process.env.AUTH_API_URL || "http://localhost:8082/api",
-    provider: {
-      type: "local",
-      endpoints: {
-        signIn: { path: "/login", method: "post" },
-        signOut: { path: "/logout", method: "get" },
-        signUp: { path: "/register", method: "post" },
-        getSession: { path: "/me", method: "get" },
-      },
-      token: {
-        signInResponseTokenPointer: "/accessToken",
-      },
-      sessionDataType: {},
-    },
-  },
-  gtag: {
-    id: "G-DX7RJHR1G4",
-  },
-  svgo: {
-    autoImportPath: "../../../node_modules/circle-flags/flags/",
-  },
-  ssr: false,
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
+    css: ["maplibre-gl/dist/maplibre-gl.css", "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css", "maplibre-gl-basemaps/lib/basemaps.css"],
+    runtimeConfig: {
+      public: {
+        REALTIME_URL: process.env.REALTIME_URL || "http://geoglify.com",
+        EXP_API_URL: process.env.EXP_API_URL || "http://geoglify.com",
+        API_URL: process.env.API_URL || "http://geoglify.com/php/api",
+        MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+        OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
       },
     },
+    googleSignIn: {
+      clientId: "CLIENT ID OBTAINED FROM GOOGLE API CONSOLE",
+    },
+    modules: [
+      "@sidebase/nuxt-auth",
+      "nuxt-vue3-google-signin",
+      "nuxt3-vuex-module",
+      "nuxt-gtag",
+      "nuxt-svgo",
+      "@nuxtjs/google-fonts",
+      (_options, nuxt) => {
+        nuxt.hooks.hook("vite:extendConfig", (config) => {
+          // @ts-expect-error
+          config.plugins.push(vuetify({ autoImport: true }));
+        });
+      },
+    ],
+    auth: {
+      baseURL: "http://geoglify.com/php/api/auth",
+      provider: {
+        type: "local",
+        endpoints: {
+          signIn: { path: "/login", method: "post" },
+          signOut: { path: "/logout", method: "get" },
+          signUp: { path: "/register", method: "post" },
+          getSession: { path: "/me", method: "get" },
+        },
+        token: {
+          signInResponseTokenPointer: "/accessToken",
+        },
+        sessionDataType: {},
+      },
+    },
+    svgo: {
+      autoImportPath: "../../../node_modules/circle-flags/flags/",
+    },
+    ssr: false,
+    vite: {
+      vue: {
+        template: {
+          transformAssetUrls,
+        },
+      },
+    },
+    googleFonts: {
+      families: {
+        Nunito: {
+          // weights
+          wght: "200..1000",
+          ital: "200..1000",
+        },
+      },
+    },
   },
-  googleFonts: {
-    families: {
-      Nunito: {
-        // weights
-        wght: "200..1000",
-        ital: "200..1000",
+  $development: {
+    devtools: { enabled: true },
+    build: {
+      transpile: ["vuetify"],
+    },
+    experimental: {
+      watcher: "chokidar",
+    },
+    css: ["maplibre-gl/dist/maplibre-gl.css", "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css", "maplibre-gl-basemaps/lib/basemaps.css"],
+    runtimeConfig: {
+      public: {
+        REALTIME_URL: process.env.REALTIME_URL || "http://localhost:8080",
+        EXP_API_URL: process.env.EXP_API_URL || "http://localhost:8080",
+        API_URL: process.env.API_URL || "http://localhost:8082/api",
+        MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+        OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
+      },
+    },
+    googleSignIn: {
+      clientId: "CLIENT ID OBTAINED FROM GOOGLE API CONSOLE",
+    },
+    modules: [
+      "@sidebase/nuxt-auth",
+      "nuxt-vue3-google-signin",
+      "nuxt3-vuex-module",
+      "nuxt-gtag",
+      "nuxt-svgo",
+      "@nuxtjs/google-fonts",
+      (_options, nuxt) => {
+        nuxt.hooks.hook("vite:extendConfig", (config) => {
+          // @ts-expect-error
+          config.plugins.push(vuetify({ autoImport: true }));
+        });
+      },
+    ],
+    auth: {
+      baseURL: process.env.NUXT_AUTH_URL || "http://localhost:8082/api/auth",
+      provider: {
+        type: "local",
+        endpoints: {
+          signIn: { path: "/login", method: "post" },
+          signOut: { path: "/logout", method: "get" },
+          signUp: { path: "/register", method: "post" },
+          getSession: { path: "/me", method: "get" },
+        },
+        token: {
+          signInResponseTokenPointer: "/accessToken",
+        },
+        sessionDataType: {},
+      },
+    },
+    gtag: {
+      id: "G-DX7RJHR1G4",
+    },
+    svgo: {
+      autoImportPath: "../../../node_modules/circle-flags/flags/",
+    },
+    ssr: false,
+    vite: {
+      vue: {
+        template: {
+          transformAssetUrls,
+        },
+      },
+    },
+    googleFonts: {
+      families: {
+        Nunito: {
+          // weights
+          wght: "200..1000",
+          ital: "200..1000",
+        },
       },
     },
   },
