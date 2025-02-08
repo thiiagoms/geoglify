fe8f334ba7fee93d97b41f06a71ccddc8f8966ad
Leonel Dias <leonel.j.dias@gmail.com>
2025-02-08 23:31:04 +0000

    feat: Simplify ShipLatestPositionView model by removing fillable attributes and updating searchable index name

--- webapp/config/scout.php
+++ webapp/config/scout.php
@@ -5,42 +5,37 @@ use App\Models\ShipLatestPositionView;
 return [
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Default Search Engine
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | This option controls the default search connection that gets used while
-    | using Laravel Scout. This connection is used when syncing all models
-    | to the search service. You should adjust this based on your needs.
-    |
-    | Supported: "algolia", "meilisearch", "typesense",
-    |            "database", "collection", "null"
+    | Defines the default search connection. Typesense is specified 
+    | here, but you can use other engines like Algolia or Meilisearch.
     |
     */
 
     'driver' => env('SCOUT_DRIVER', 'typesense'),
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Index Prefix
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     |
-    | Here you may specify a prefix that will be applied to all search index
-    | names used by Scout. This prefix may be useful if you have multiple
-    | "tenants" or applications sharing the same search infrastructure.
+    | Define a prefix for your search indexes. Useful for multi-tenancy 
+    | or shared search infrastructure.
     |
     */
 
     'prefix' => env('SCOUT_PREFIX', ''),
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Queue Data Syncing
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | This option allows you to control if the operations that sync your data
-    | with your search engines are queued. When this is set to "true" then
-    | all automatic data syncing will get queued for better performance.
+    | Defines whether operations that sync data with the search engine 
+    | are queued. Set to false for direct syncing if performance isn't 
+    | critical.
     |
     */
 
@@ -50,26 +45,24 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Database Transactions
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | This configuration option determines if your data will only be synced
-    | with your search indexes after every open database transaction has
-    | been committed, thus preventing any discarded data from syncing.
+    | Defines if data sync with search engine happens only after database 
+    | transactions are committed.
     |
     */
 
     'after_commit' => false,
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Chunk Sizes
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | These options allow you to control the maximum chunk size when you are
-    | mass importing data into the search engine. This allows you to fine
-    | tune each of these chunk sizes based on the power of the servers.
+    | Defines chunk sizes for mass importing data. This is important 
+    | for performance when syncing large datasets.
     |
     */
 
@@ -79,80 +72,36 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Soft Deletes
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | This option allows to control whether to keep soft deleted records in
-    | the search indexes. Maintaining soft deleted records can be useful
-    | if your application still needs to search for the records later.
+    | Defines whether soft-deleted records are maintained in the search 
+    | index. For read-only views, soft deletes are not needed.
     |
     */
 
     'soft_delete' => false,
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Identify User
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | This option allows you to control whether to notify the search engine
-    | of the user performing the search. This is sometimes useful if the
-    | engine supports any analytics based on this application's users.
-    |
-    | Supported engines: "algolia"
+    | If true, search engines (e.g., Algolia) will identify the user 
+    | performing searches. This is useful for analytics, but not 
+    | applicable for Typesense.
     |
     */
 
     'identify' => env('SCOUT_IDENTIFY', false),
 
     /*
-    |--------------------------------------------------------------------------
-    | Algolia Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your Algolia settings. Algolia is a cloud hosted
-    | search engine which works great with Scout out of the box. Just plug
-    | in your application ID and admin API key to get started searching.
-    |
-    */
-
-    'algolia' => [
-        'id' => env('ALGOLIA_APP_ID', ''),
-        'secret' => env('ALGOLIA_SECRET', ''),
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Meilisearch Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your Meilisearch settings. Meilisearch is an open
-    | source search engine with minimal configuration. Below, you can state
-    | the host and key information for your own Meilisearch installation.
-    |
-    | See: https://www.meilisearch.com/docs/learn/configuration/instance_options#all-instance-options
-    |
-    */
-
-    'meilisearch' => [
-        'host' => env('MEILISEARCH_HOST', 'http://localhost:7700'),
-        'key' => env('MEILISEARCH_KEY'),
-        'index-settings' => [
-            // 'users' => [
-            //     'filterableAttributes'=> ['id', 'name', 'email'],
-            // ],
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Typesense Configuration
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------- 
     |
-    | Here you may configure your Typesense settings. Typesense is an open
-    | source search engine using minimal configuration. Below, you will
-    | state the host, key, and schema configuration for the instance.
+    | Typesense search engine configuration.
     |
     */
 
@@ -178,60 +127,29 @@ return [
             'num_retries' => env('TYPESENSE_NUM_RETRIES', 3),
             'retry_interval_seconds' => env('TYPESENSE_RETRY_INTERVAL_SECONDS', 1),
         ],
-        'max_total_results' => env('TYPESENSE_MAX_TOTAL_RESULTS', 1000),
         'model-settings' => [
             ShipLatestPositionView::class => [
                 'collection-schema' => [
                     'fields' => [
+                        ['name' => 'id', 'type' => 'string'],
+                        ['name' => 'imo', 'type' => 'string'],
+                        ['name' => 'mmsi', 'type' => 'string'],
+                        ['name' => 'name', 'type' => 'string'],
+                        ['name' => 'callsign', 'type' => 'string'],
+                        ['name' => 'destination', 'type' => 'string'],
+                        ['name' => 'cargo_type_name', 'type' => 'string'],
+                        ['name' => 'cargo_category_name', 'type' => 'string'],
+                        ['name' => 'country_name', 'type' => 'string'],
+                        ['name' => 'country_iso_code', 'type' => 'string'],
                         [
-                            'name' => 'id',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'mmsi',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'name',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'callsign',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'imo',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'cargo_name',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'destination',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'country_iso_code',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'country_name',
-                            'type' => 'string',
-                        ],
-                        [
-                            'name' => 'created_at',
-                            'type' => 'int64',
-                        ],
-                        [
-                            'name' => 'updated_at',
-                            'type' => 'int64',
+                            'name' => '__soft_deleted',
+                            'type' => 'int32',
+                            'optional' => true,
                         ],
                     ],
-                    'default_sorting_field' => 'updated_at',
                 ],
                 'search-parameters' => [
-                    'query_by' => 'name,imo,callsign,mmsi,destination,country_name,cargo_name',
+                    'query_by' => 'imo,mmsi,name,callsign,destination,cargo_type_name,cargo_category_name,country_name,country_iso_code',
                 ],
             ],
         ],
